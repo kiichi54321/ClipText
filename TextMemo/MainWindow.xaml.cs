5421f9206ce57f573fdcb17100a02a4a3b90c93c
伊藤貴一 <takaichi.ito@gmail.com>
2015-12-10 22:37:39 +0900

    一応の完成。

---
+++ TextMemo/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextMemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MouseLeftButtonDown += (sender, e) => this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = !this.Topmost;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            textblock.Text = Clipboard.GetText();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            textblock.Text = string.Empty;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
